0c8811c271c034e2ae2a18e864b0ddc5709a24d8
Swati <swati308mmu@gmail.com>
2021-11-12 19:42:00 +0530

    directory restructured

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
-from apps import raw, tableau_dashboard, problem_statement, visualisations, transformation, model
+from apps import raw, tableau_dashboard, problem_statement, visualisations
 
 
 
-choice = st.sidebar.selectbox("Select",("Problem Statement","Raw Dataset and Cleaned Dataset", "Operations on Cleaned Dataset", "Tableau Dashboard", "Google PlayStore Rating Prediction App"))
+choice = st.sidebar.selectbox("Select",("Problem Statement","Raw Dataset and Cleaned Dataset", "Operations on Cleaned Dataset", "Tableau Dashboard"))
 if choice == "Problem Statement":
     problem_statement.writeup()
 elif choice == "Raw Dataset and Cleaned Dataset":
     raw.Raw_Data().run()
 elif choice == "Operations on Cleaned Dataset":
     visualisations.Analysis().run()
-elif choice == "Tableau Dashboard":
-    tableau_dashboard.display_tableauReport()
 else:
-    #transformation.Transform_Train().run()
-    model.run()
+    tableau_dashboard.display_tableauReport()
+# else:
+#     #transformation.Transform_Train().run()
+#     model.run()
